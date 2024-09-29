fc16fb1ec942cfe461aaa42f609d57b361fc64ba
HabibRouatbi <hebib@camelt-soft.com>
2024-09-29 13:24:38 +0100

    submitted to leetcode

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+   int[] nums = {2,7,11,15} ;
+    int[] out = new int[2] ;
+    int tar = 2 ;
+    for (int i = 0  ; i < nums.length ; i++ ) {
+        for (int j= i+1 ; j < nums.length ; j++ ) {
+            if(nums[i] +nums[j] == tar) {
+                out[0] = i ;
+                out[1] = j ;
+            }
+
+        }
+
+
+    }
+    System.out.println(Arrays.toString(out));
+
     }
 }
