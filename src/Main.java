5a8d3670624d1e3a7b8bc82edb68b171283f4da2
HabibRouatbi <hebib@camelt-soft.com>
2024-10-03 11:54:57 +0100

    submitted to leetcode

--- src/Main.java
+++ src/Main.java
@@ -2,29 +2,13 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        boolean isHappy = isHappy(19) ;
-        System.out.print(isHappy);
-
-
     }
-    static boolean isHappy(int n)
-    {
-        int l=0,sum=0;
-        while(n>0)
-        {
-            l=n%10;
-            sum=sum+(l*l);
-            n=n/10;
+    public int singleNumber(int[] nums) {
+        int xor = 0;
+        for(int i = 0; i < nums.length; i++){
+            xor ^= nums[i];
         }
-        if (sum==1)
-            return true;
-        else if(sum==7) //only one happy number(7) b/w 1 and 10
-            return true;
-        else if(sum<10)
-            return false;
-        else
-            return isHappy(sum);
 
-
+        return xor;
     }
 }
