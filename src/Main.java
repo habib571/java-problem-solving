fd8014c94b2fc9154e80e3702389e6a4bc787550
HabibRouatbi <hebib@camelt-soft.com>
2024-10-10 13:15:32 +0100

    submitted to leetcode

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,30 @@
+import java.math.BigInteger;
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-    }
-    public int singleNumber(int[] nums) {
-        int xor = 0;
-        for(int i = 0; i < nums.length; i++){
-            xor ^= nums[i];
+        int[] digits = {9}; // Example input
+        int[] plusOne = new int[digits[0] != 9 ? digits.length : digits.length + 1];
+        StringBuilder ch = new StringBuilder();
+
+        for (int i : digits) {
+            ch.append(i);
         }
 
-        return xor;
+        BigInteger number = new BigInteger(ch.toString());
+        BigInteger sum = number.add(BigInteger.ONE);
+
+        String result = sum.toString();
+
+        for (int i = 0; i < result.length(); i++) {
+            plusOne[i] = Character.getNumericValue(result.charAt(i));
+        }
+
+        System.out.println(Arrays.toString(plusOne));
+
+
+
     }
-}
+
+
+}   
