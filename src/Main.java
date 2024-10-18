7a562009d86a871c4af173c1d1ab4462ae527804
HabibRouatbi <hebib@camelt-soft.com>
2024-10-18 12:01:14 +0100

    submitted to leetcode

--- src/Main.java
+++ src/Main.java
@@ -3,28 +3,24 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] digits = {9}; // Example input
-        int[] plusOne = new int[digits[0] != 9 ? digits.length : digits.length + 1];
-        StringBuilder ch = new StringBuilder();
 
-        for (int i : digits) {
-            ch.append(i);
+
+}
+    public ListNode removeElements(ListNode head, int val) {
+        while (head != null && head.val == val) {
+            head = head.next;
         }
 
-        BigInteger number = new BigInteger(ch.toString());
-        BigInteger sum = number.add(BigInteger.ONE);
-
-        String result = sum.toString();
-
-        for (int i = 0; i < result.length(); i++) {
-            plusOne[i] = Character.getNumericValue(result.charAt(i));
+        ListNode curr = head;
+        while (curr != null && curr.next != null) {
+            if (curr.next.val == val) {
+                curr.next = curr.next.next;
+            } else {
+                curr = curr.next;
+            }
         }
 
-        System.out.println(Arrays.toString(plusOne));
-
-
-
+        return head;
     }
+}
 
-
-}   
